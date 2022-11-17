bd4767b19aaf05d4a2983d6fb62555937c17e13e
Nelson Sanchez <n.sanchezesparza@gmail.com>
2022-11-16 20:37:57 -0500

    creaciÃon de aplicacion

---
+++ src/main/java/com/grupo1a/grupo1abackend/Grupo1ABackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1a.grupo1abackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1ABackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo1ABackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo1a/grupo1abackend/Grupo1ABackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1a.grupo1abackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1ABackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
